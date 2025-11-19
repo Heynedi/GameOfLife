2bfe6e3021ea1a6418014800872870cb71447f45
elioesca <125869773+elioesca@users.noreply.github.com>
2025-11-19 10:35:26 +0100

    Change matrice to bool and add afficherSFML method
    Updated the matrice type to use bool for cell states and added a new display method for SFML.

--- grille.hpp
+++ grille.hpp
@@ -1,22 +1,18 @@
 #ifndef GRILLE_HPP
 #define GRILLE_HPP
-
 #include <vector>
 #include <SFML/Graphics.hpp>
+
 class Grille {
 private:
     int lignes;
     int colonnes;
-    std::vector<std::vector<char>> matrice; // Je crée une matrice composée de plusieurs lignes et chaque ligne contient plusieurs caractères.”
+    std::vector<std::vector<bool>> matrice;  // Bool pour vivant (true) ou mort (false)
+    const int cellSize = 20;  // Taille d'un carré en pixels
 
 public:
-    // Constructeur
     Grille(int lignes, int colonnes);
-
-    // Affichage
-    void afficher() const;
-// le const : Cette méthode n’a pas le droit de modifier les attributs de l’objet.
-    // Accesseurs
+    void afficherSFML(sf::RenderWindow &window) const;
     int getLignes() const { return lignes; }
     int getColonnes() const { return colonnes; }
 };
