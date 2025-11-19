6ec7c670d841395e4fb01af48a33af63a35c8147
elioesca <125869773+elioesca@users.noreply.github.com>
2025-11-19 10:35:02 +0100

    Integrate SFML for grid display in main function
    Updated main function to use SFML for rendering a grid instead of console input.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include "grille.hpp"
+#include <SFML/Graphics.hpp>
 
 int main() {
-    int l, c;
+    // Taille par défaut : 30 lignes, 31 colonnes
+    const int lignes = 30;
+    const int colonnes = 31;
+    const int cellSize = 20;  // Doit correspondre à celle dans Grille et permet de mettre la taille de la case
 
-    std::cout << "Entrez le nombre de lignes : ";
-    std::cin >> l;
+    Grille grille(lignes, colonnes);
 
-    std::cout << "Entrez le nombre de colonnes : ";
-    std::cin >> c;
+    // Créer une fenêtre SFML
+    sf::RenderWindow window(sf::VideoMode({// sf::VideoMode définit la taille de la fenêtre (largeur, hauteur)
+        static_cast<unsigned int>(colonnes * cellSize), // Ici, on calcule dynamiquement la taille pour s'adapter à la grille Largeur = nombre de colonnes * taille d'une cellule ,Hauteur = nombre de lignes * taille d'une cellule
+        static_cast<unsigned int>(lignes * cellSize) // on change en unsigned int car la fonction attend ceci
+    }), "Jeu de la Vie - Grille");// nom qui est affiché dans le haut de la grille
 
-    Grille grille(l, c);
+    while (window.isOpen()) { // boucle pour savoir quand la fenetre est encore ouverte
+        while (const std::optional event = window.pollEvent()) { // permet de gérer les évènements (clicks ou autre...) et le stocke dans event
+            if (event->is<sf::Event::Closed>())// si l'event est de fermer la fenetre alors on ferme la fenetre
+                window.close();
+        }
 
-    std::cout << "\nVoici votre grille :\n";
-    grille.afficher();
+        // Appel à la méthode d'affichage de la grille
+        grille.afficherSFML(window);
+    }
 
     return 0;
 }
